a370fe6eb31d9d2ed987b5102bee39198909ddb5
sim <s5.pentin@yandex.ru>
2023-11-22 20:20:50 +0300

    add tgDataCheck and casandraConfig

---
+++ tg-bot/src/main/java/polytech/CassandraConfig.java
@@ -0,0 +1,59 @@
+package polytech;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
+import org.springframework.data.cassandra.config.SchemaAction;
+
+@Configuration
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.data.cassandra.contact-points}")
+    private String contactPoints;
+
+    @Value("${spring.data.cassandra.port}")
+    private int port;
+
+    @Value("${spring.data.cassandra.keyspace-name}")
+    private String keySpace;
+
+    @Value("${spring.data.cassandra.username}")
+    private String username;
+
+    @Value("${spring.data.cassandra.password}")
+    private String password;
+
+    @Value("${spring.data.cassandra.schema-action}")
+    private String schemaAction;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keySpace;
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.valueOf(schemaAction);
+    }
+
+    @Bean
+    @Override
+    public CqlSessionFactoryBean cassandraSession() {
+        CqlSessionFactoryBean cassandraSession = super.cassandraSession();
+        cassandraSession.setUsername(username);
+        cassandraSession.setPassword(password);
+        return cassandraSession;
+    }
+}
